bc7caa4d7922d88fb04d6208609969e501151f22
Manjot Singh <sin13014.sheridancollege.ca>
2023-07-23 22:33:17 -0400

    added default image for urlToImage=null from APi

--- src/components/NewsItem/NewsItem.jsx
+++ src/components/NewsItem/NewsItem.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 import "./newsItem.css";
-
+import defaultImage from "../../media/defaultNews.jpg";
 export class NewsItem extends Component {
   render() {
     const { title, description, urlToImage, url } = this.props.article;
 
     return (
-      <div className="card" >
-        <img src={urlToImage} className="card-img-top" alt="..." />
+      <div className="card">
+        <img
+          src={urlToImage ? urlToImage : defaultImage}
+          className="card-img-top"
+          alt="..."
+        />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className=" card-text">{description}</p>
-          <a href={url} target="_blank" className="button">
+          <a href={url} target="_blank" rel="noreferrer" className="button">
             Read More
           </a>
         </div>
