74070e62e91be76d1fce90534dc47392ae986b72
Manjot Singh <sin13014.sheridancollege.ca>
2023-07-23 22:54:06 -0400

    Chnaged Api from NewsApi to mediastack

--- src/components/NewsContainer/News.jsx
+++ src/components/NewsContainer/News.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
 import NewsItem from "../NewsItem/NewsItem";
-import newsData from "../../example.json";
 import "./newsContainer.css";
 
 export class News extends Component {
-  newsArticles = newsData.articles;
   constructor() {
     super();
-    this.state = { articles: this.newsArticles, loading: false };
+    this.state = { articles: [], loading: false, page: 1 };
   }
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bc9af3735736477a953fd7c0d3beb4a4";
+      "http://api.mediastack.com/v1/news?access_key=f553f07d194bd9848f3ca5b8689d9141";
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles });
+    this.setState({ articles: parsedData.data });
   }
   render() {
     const newsItems = this.state.articles.map((article) => {
