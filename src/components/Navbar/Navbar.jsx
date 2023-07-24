4f6db4896513595870c124d079a6ef40fd8132d3
Manjot Singh <sin13014.sheridancollege.ca>
2023-07-23 21:56:03 -0400

    Styled navbar and made it responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,39 +1,25 @@
 import React, { Component } from "react";
-
+import "./navbar.css";
 export class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            News Pigeon
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link " aria-current="page" href="/about">
-                  About
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <nav className="navbar">
+        <input hidden type="checkbox" id="menu" />
+        <label for="menu" class="material-symbols-outlined burger-icon">
+          menu
+        </label>
+        <label className="logo">NewsPigeon</label>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Explore</a>
+          </li>
+        </ul>
       </nav>
     );
   }
