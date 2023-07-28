d54552bda5b0802f0702402bce2e1efa9f487bb1
Manjot Singh <sin13014.sheridancollege.ca>
2023-07-28 02:24:42 -0400

    Added: a new Logo and name, a new theme color and a search element

--- src/components/HeroComponent/Hero.jsx
+++ src/components/HeroComponent/Hero.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
 import data from "./backupData.json";
 import "./hero.css";
 import {
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,39 @@
 import React, { Component } from "react";
 import "./navbar.css";
+import logo from "../../media/logo.png";
+import burgerIcon from "../../media/burger-icon.png";
+import { FaSearch, FaFilter } from "react-icons/fa";
 export class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar">
+      <nav className="navigation-bar a-center d-flex">
         <input hidden type="checkbox" id="menu" />
-        <label htmlFor="menu" className="material-symbols-outlined burger-icon">
-        
-        </label>
-        <label className="logo f-ubuntu">Tanjiro</label>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/">About</a>
-          </li>
-          <li>
-            <a href="/">Explore</a>
-          </li>
-        </ul>
+        <div className="menu-group a-center d-flex">
+          <img src={burgerIcon} className="menu-icon" alt="bugerr-icon" />
+          <div className="logo-wrapper a-center d-flex">
+            <img src={logo} className="logo" alt="logo" />
+            <h2 className="logo-text">
+              war<span className="logo-text-red">Ding</span>
+            </h2>
+          </div>
+        </div>
+        <div className="search-wrapper">
+          <input
+            type="text"
+            className="search-text f-poppins"
+            placeholder="Search anime..."
+          />
+          <FaSearch
+            className="search-icon search-icons"
+            size={20}
+            color="grey"
+          />
+          <FaFilter
+            className="filter-icon search-icons"
+            size={20}
+            color="grey"
+          />
+        </div>
       </nav>
     );
   }
