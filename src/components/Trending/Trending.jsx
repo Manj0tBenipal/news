0f8fa36acec4e25c581713c312b368e87e1cd110
Manjot Singh <sin13014.sheridancollege.ca>
2023-07-30 02:19:44 -0400

    trending section added

---
+++ src/components/Trending/Trending.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./trending.css";
+export default function Trending() {
+  return (
+    <div className="tranding-section-wrapper">
+      <h2 className="section-header">Trending</h2>
+    </div>
+  );
+}
