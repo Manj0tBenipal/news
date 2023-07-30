5a8aafbf5ed9b15863d0328df8c30fbb8d46590c
Manjot Singh <sin13014.sheridancollege.ca>
2023-07-30 01:45:04 -0400

    Navbar And hero adjusted for small screens. Extra files removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import "./App.css";
 import Hero from "./components/HeroComponent/Hero";
 import Navbar from "./components/Navbar/Navbar";
-import React, { Component } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Navbar />
-        <Hero />
-      </div>
-    );
-  }
+export default function App() {
+  const containerRef = useRef(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    function handleScroll() {
+      if (container) {
+        const scrollPosition = container.getBoundingClientRect().top;
+        console.log(scrollPosition);
+        setScrollPosition(scrollPosition);
+      }
+    }
+    container.addEventListener("scroll", handleScroll);
+
+    return () => {
+      container.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <div ref={containerRef} className="container">
+      <Navbar isScrolled={scrollPosition > 0} />
+      <Hero />
+    </div>
+  );
 }
