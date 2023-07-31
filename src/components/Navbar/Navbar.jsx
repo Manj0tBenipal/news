66e284d6412248f29be1346cb15a232739c16038
Manjot Singh <sin13014.sheridancollege.ca>
2023-07-31 13:12:31 -0400

    Side menu animation added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import logo from "../../media/logo.png";
-import burgerIcon from "../../media/burger-icon.png";
 import logoText from "../../media/logo-text.png";
 import profileIcon from "../../media/profile.jpg";
 import {
@@ -16,65 +15,85 @@ import {
   FaComments,
   FaBell,
   FaTwitter,
-  FaStream,
   FaBroadcastTower,
 } from "react-icons/fa";
+import NavSidebar from "./NavSidebar";
 export default function NavBar() {
+ const [sidebarIsOpen, setSidebarIsOpen] =  useState(false);
   return (
-    <nav className="navigation-bar a-center d-flex">
-      <input hidden type="checkbox" id="menu" />
-      <div className="menu-group a-center d-flex">
-        <FaBars size={20} className="burger-icon trans-05"/>
-        <div className="logo-wrapper a-center d-flex">
-          <img src={logo} className="logo" alt="logo" />
-          <img src={logoText} className="logo-text" alt="logo-text" />
+    <>
+      <NavSidebar
+      sidebarIsOpen={sidebarIsOpen}
+      setSidebarIsOpen={setSidebarIsOpen}
+      />
+      <nav className="navigation-bar a-center d-flex">
+      
+
+        <div className="menu-group a-center d-flex">
+          <FaBars
+            size={20}
+            className="burger-icon trans-05"
+            onClick={()=>setSidebarIsOpen(true)}
+          />
+          <div className="logo-wrapper a-center d-flex">
+            <img src={logo} className="logo" alt="logo" />
+            <img src={logoText} className="logo-text" alt="logo-text" />
+          </div>
         </div>
-      </div>
-      <div className="search-wrapper">
-        <input
-          type="text"
-          className="search-text f-poppins"
-          placeholder="Search anime..."
-        />
-        <FaSearch className="search-icon search-icons" size={20} color="grey" />
-        <FaFilter className="filter-icon search-icons" size={20} color="grey" />
-      </div>
-      <div className="social-links-wrapper">
-        <span style={{ backgroundColor: "#6f85d5" }}>
-          <FaDiscord size={25} />
-        </span>
-        <span style={{ backgroundColor: "#ff3c1f" }}>
-          <FaRedditAlien size={25} />
-        </span>
-        <span style={{ backgroundColor: "#08c" }}>
-          <FaTelegramPlane size={25} />
-        </span>
-        <span style={{ backgroundColor: "#1d9bf0" }}>
-          <FaTwitter size={25} />
-        </span>
-      </div>
-      <div className="nav-actions f-poppins text-light trans-c-03">
-        <span>
-          <FaBroadcastTower size={20} />
-          <p>Watch Togather</p>
-        </span>
-        <span>
-          <FaRandom size={20} />
-          <p>Random</p>
-        </span>
-        <span>
-          <FaLanguage size={20} />
-          <p>Anime Name</p>
-        </span>
-        <span>
-          <FaComments size={20} />
-          <p>Community</p>
-        </span>
-      </div>
-      <div className="user-profile-nots a-center j-center d-flex trans-c-03">
-        <FaBell size={20} />
-        <img className="profile-icon" src={profileIcon} />
-      </div>
-    </nav>
+        <div className="search-wrapper">
+          <input
+            type="text"
+            className="search-text f-poppins"
+            placeholder="Search anime..."
+          />
+          <FaSearch
+            className="search-icon search-icons"
+            size={20}
+            color="grey"
+          />
+          <FaFilter
+            className="filter-icon search-icons"
+            size={20}
+            color="grey"
+          />
+        </div>
+        <div className="social-links-wrapper">
+          <span style={{ backgroundColor: "#6f85d5" }}>
+            <FaDiscord size={25} />
+          </span>
+          <span style={{ backgroundColor: "#ff3c1f" }}>
+            <FaRedditAlien size={25} />
+          </span>
+          <span style={{ backgroundColor: "#08c" }}>
+            <FaTelegramPlane size={25} />
+          </span>
+          <span style={{ backgroundColor: "#1d9bf0" }}>
+            <FaTwitter size={25} />
+          </span>
+        </div>
+        <div className="nav-actions f-poppins text-light trans-c-03">
+          <span>
+            <FaBroadcastTower size={20} />
+            <p>Watch Togather</p>
+          </span>
+          <span>
+            <FaRandom size={20} />
+            <p>Random</p>
+          </span>
+          <span>
+            <FaLanguage size={20} />
+            <p>Anime Name</p>
+          </span>
+          <span>
+            <FaComments size={20} />
+            <p>Community</p>
+          </span>
+        </div>
+        <div className="user-profile-nots a-center j-center d-flex trans-c-03">
+          <FaBell size={20} />
+          <img className="profile-icon" src={profileIcon} alt="profile-icon" />
+        </div>
+      </nav>
+    </>
   );
 }
